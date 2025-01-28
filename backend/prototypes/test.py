c1fc3e40376a63667d5c1ef4cd44bf598ba12f94
willthewa <willthewa@gmail.com>
2025-01-27 17:37:26 -0800

    added prototype folder

---
+++ backend/prototypes/test.py
@@ -0,0 +1,9 @@
+# Speech Recognition
+# pip install SpeechRecognition
+import SpeechRecognition as sr
+
+recognizer = sr.Recognizer()
+with sr.AudioFile('test.wav') as source:
+    audio_data = recognizer.record(source)
+    text = recognizer.recognize_google(audio_data)
+    print(text)
