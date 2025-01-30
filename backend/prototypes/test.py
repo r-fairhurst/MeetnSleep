a25d55f587416bf19d97e23eedea1b4931d1488d
mortonwi <mortonwi@oregonstate.edu>
2025-01-29 17:17:09 -0800

    Update basic test script

--- backend/prototypes/test.py
+++ backend/prototypes/test.py
@@ -3,10 +3,18 @@
 
 # Use microphone instead of audio file 
 
-import SpeechRecognition as sr
+import pyaudio
+import wave
+import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 with sr.AudioFile('test.wav') as source:
     audio_data = recognizer.record(source)
-    text = recognizer.recognize_google(audio_data)
-    print(text)
+
+    try:
+        text = recognizer.recognize_google(audio_data)
+        print("Transcript:", text)
+    except sr.UnknownValueError:
+        print("Could not understand the audio.")
+    except sr.RequestError:
+        print("Could not request results from Google Speech Recognition.")
