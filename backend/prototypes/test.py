56fd967e7b5f632e9084c2eb4acd263746d261fe
mortonwi <mortonwi@oregonstate.edu>
2025-01-29 17:30:12 -0800

    Updated test prototype

--- backend/prototypes/test.py
+++ backend/prototypes/test.py
@@ -1,20 +1,33 @@
-# Speech Recognition
-# Dependencies: pip install SpeechRecognition
+# Dependencies: 
+#   pip install SpeechRecognition
+#   pip install pyaudio
 
-# Use microphone instead of audio file 
-
+import sys
 import pyaudio
 import wave
 import speech_recognition as sr
 
-recognizer = sr.Recognizer()
-with sr.AudioFile('test.wav') as source:
-    audio_data = recognizer.record(source)
-
+def transcribe_audio(filename):
     try:
-        text = recognizer.recognize_google(audio_data)
-        print("Transcript:", text)
-    except sr.UnknownValueError:
-        print("Could not understand the audio.")
-    except sr.RequestError:
-        print("Could not request results from Google Speech Recognition.")
+        recognizer = sr.Recognizer()
+        with sr.AudioFile(filename) as source:
+            audio_data = recognizer.record(source)
+
+            try:
+                text = recognizer.recognize_google(audio_data)
+                print("Transcript:", text)
+            except sr.UnknownValueError:
+                print("ERROR: Could not understand the audio.")
+            except sr.RequestError:
+                print("ERROR: Could not request results from Google Speech Recognition.")
+    except FileNotFoundError:
+        print("ERROR: File " + filename + " was not found.")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("ERROR: No audio file provided.")
+        print("USAGE: python test.py <audio_file>")
+        sys.exit(1)
+    filename = sys.argv[1]
+    
+    transcribe_audio(filename)
